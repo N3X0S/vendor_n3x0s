fcce849b032459e41336f4d4b0c98f47132f3cd0
Shakir Abdul-Ahad <mmc1971@att.net>
2015-07-11 06:53:27 -0700

    Set qdisc to CAKE by default. Credit: Mattew Alex (DespairFactor) for backporting this, since no one else knew how to!!

--- extras/device-partial.mk
+++ extras/device-partial.mk
@@ -1,6 +1,7 @@
 # Extra Packages and Files
 PRODUCT_COPY_FILES += \
     vendor/n3x0s/extras/etc/init.d/99fstrim:system/etc/init.d/99fstrim:extras \
+    vendor/n3x0s/extras/etc/init.d/99qdisc:system/etc/init.d/99qdisc \
     vendor/n3x0s/extras/supersu/SuperSU.zip:system/supersu/SuperSU.zip \
     vendor/n3x0s/extras/supersu/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
